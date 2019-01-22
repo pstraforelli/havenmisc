4dbb339bb84988643ddf633ace4978d0bee73ebb
Phil Straforelli <pstraforelli@gmail.com>
2019-01-22 13:38:35 -0800

    Finalizing v0.1.

--- R/labels_df.R
+++ R/labels_df.R
@@ -6,10 +6,12 @@
 #' @export
 #'
 #' @examples
+#' gss_data <- haven::read_sav(system.file("extdata", "GSS2016.sav", package = "havenmisc"))
+#' labels_df(gss_data)
 
 labels_df <- function(df) {
   labels <- labelled::var_label(df)
   long_df <- purrr::flatten_df(labels) 
-  output <- tidyr::gather(long_df, key = name, value = label)
+  output <- tidyr::gather(long_df, key = !! rlang::sym("name"), value = !! rlang::sym("label"))
   output
 }
